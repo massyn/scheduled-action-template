b342dfa8a5a680fa8da13657a14f880949314032
Phil Massyn <massyn@users.noreply.github.com>
2024-10-19 13:04:17 +1000

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-content = f"# Hello, World!\n\nThis script was generated on: {current_time}"
+content = f"# Hello, World!\n\nThis script was generated on: {current_time}\n\n[![Daily Build](https://github.com/massyn/scheduled-action-template/actions/workflows/python-app.yml/badge.svg)](https://github.com/massyn/scheduled-action-template/actions/workflows/python-app.yml)"
 
 with open("README.md", "w") as file:
     file.write(content)
